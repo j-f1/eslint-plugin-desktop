a87436ee55845bdaa7c10130b5016ca273a89d74
Jed Fox <git@twopointzero.us>
2017-09-10 11:46:55 -0400

    desktop/react-lifecycle-method-syntax ✨

--- README.md
+++ README.md
@@ -67,4 +67,5 @@ Then configure the rules you want to use under the rules section.
 * [`desktop/no-stateless-class`](./docs/rules/no-stateless-class.md) — Forbid classes without state
 * [`desktop/promise-must-complete`](./docs/rules/promise-must-complete.md) — When a Promise is created with `new Promise`, the function provided must call `reject()` or `resolve()` in all branches.
 * [`desktop/react-bind-event-handlers`](./docs/rules/react-bind-event-handlers.md) — Ensure that methods passed to components are bound
+* [`desktop/react-lifecycle-method-syntax`](./docs/rules/react-lifecycle-method-syntax.md) — Forbid erroneous usage of React lifecycle methods
 <!-- end rule list -->
---
+++ tests/lib/rules/react-lifecycle-method-syntax.js
@@ -0,0 +1,138 @@
+/**
+ * @fileoverview Forbid erroneous usage of React lifecycle methods
+ * @author Jed Fox
+ */
+'use strict'
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/react-lifecycle-method-syntax')
+const RuleTester = require('eslint').RuleTester
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester({
+  parser: 'typescript-eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+})
+ruleTester.run('react-lifecycle-method-syntax', rule, {
+  valid: [
+    `
+      class Foo extends React.Component<P, S> {
+        componentWillMount() {}
+        componentDidMount() {}
+        componentWillUnmount() {}
+
+        componentWillReceiveProps() {}
+
+        componentWillUpdate() {}
+        componentDidUpdate() {}
+
+        shouldComponentUpdate()
+      }
+    `,
+    `
+      class Foo extends React.Component<P, S> {
+        componentWillMount() {}
+        componentDidMount() {}
+        componentWillUnmount() {}
+
+        componentWillReceiveProps(nextProps: P) {}
+
+        componentWillUpdate(nextProps: P) {}
+        componentDidUpdate(prevProps: P) {}
+
+        shouldComponentUpdate(nextProps: P)
+      }
+    `,
+    `
+      class Foo extends React.Component<P, S> {
+        componentWillMount() {}
+        componentDidMount() {}
+        componentWillUnmount() {}
+
+        componentWillReceiveProps(nextProps: P) {}
+
+        componentWillUpdate(nextProps: P, nextState: S) {}
+        componentDidUpdate(prevProps: P, prevState: S) {}
+
+        shouldComponentUpdate(nextProps: P, nextState: S)
+      }
+    `,
+  ],
+
+  invalid: [
+    {
+      code: `
+        class Foo extends React.Component<P, S> {
+          componentWillMount(foo: Bar) {}
+        }
+      `,
+      errors: [
+        {
+          message: 'componentWillMount should not accept any parameters.',
+          line: 3,
+          column: 11,
+        },
+      ],
+    },
+    {
+      code: `
+        class Foo extends React.Component<P, S> {
+          componentWillReceiveProps(nextProps: P, nextState: S) {}
+        }
+      `,
+      errors: [
+        {
+          message: 'Unknown parameter `nextState`',
+          line: 3,
+          column: 51,
+        },
+      ],
+    },
+    {
+      code: `
+        class Foo extends React.Component<P, S> {
+          componentWillReceiveProps(foo: Foo) {}
+        }
+      `,
+      output: `
+        class Foo extends React.Component<P, S> {
+          componentWillReceiveProps(foo: P) {}
+        }
+      `,
+      errors: [
+        {
+          message: 'The foo parameter should be named nextProps.',
+          line: 3,
+          column: 37,
+        },
+        {
+          message: 'The foo parameter should have a type annotation of `P`',
+          line: 3,
+          column: 42,
+        },
+      ],
+    },
+  ],
+})
+
+/*
+componentDidMount() {}
+componentWillUnmount() {}
+
+componentWillReceiveProps(nextProps: P) {}
+
+componentWillUpdate(nextProps: P, nextState: S) {}
+componentDidUpdate(prevProps: P, prevState: S) {}
+
+shouldComponentUpdate(nextProps: P, nextState: S)
+*/
